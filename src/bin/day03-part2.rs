f998be1b0a80ea3277b5f332ff338033bce84dc0
Prabu Natarajan <prabuinet@gmail.com>
2023-12-03 09:30:42 +0530

    day03-part2 cleanup

--- src/bin/day03-part2.rs
+++ src/bin/day03-part2.rs
@@ -37,26 +37,8 @@ fn main() {
 
     while line_number < lines.len() {
         let positions = get_nums(lines.get(line_number).unwrap());
-        let line = lines[line_number].to_string();
-
         num_positions.push(positions);
-
-        // print!("{}   ", line);
-        // for pos in num_positions {
-        //     // let num = (&line[pos.0 as usize .. (pos.1 + 1) as usize]).parse::<i32>();
-        //     // println!("{}", num.unwrap());
-
-        //     if is_part_number(line_number, pos, &lines) {
-        //         let num = (&line[pos.0 as usize .. (pos.1 + 1) as usize]).parse::<i32>();
-        //         if let Ok(n) = num {
-        //             sum += n;
-        //             print!("{:?},", n);
-        //         }
-        //     }
-        // }
-
         line_number += 1;
-        // println!("");
     }
 
     line_number = 0;
@@ -65,7 +47,7 @@ fn main() {
 
         let mut j = 0;
 
-        print!("{}:    ", lines[line_number]);
+        // print!("{}:    ", lines[line_number]);
 
         while j < line.len() {
             if line.chars().nth(j).unwrap() == '*' {
@@ -74,23 +56,18 @@ fn main() {
                     sum += gear_nums[0] as u128 * gear_nums[1] as u128;
                 }
 
-                print!("{:?}", gear_nums);
-                // println!("{}", g);
-                // sum += g as u128;
+                // print!("{:?}", gear_nums);
             }
 
             j += 1;
         }
 
-        println!("");
+        // println!("");
         line_number += 1;
     }
 
 
     println!("{}", sum);
-
-    // dbg!(result);
-    // println!("{}", line);
 }
 
 fn get_gear(lines: &Vec<&str>, num_positions: &Vec<Vec<(u32, u32)>>, line_number: usize, col: usize) -> Vec<u64> {
@@ -109,12 +86,6 @@ fn get_gear(lines: &Vec<&str>, num_positions: &Vec<Vec<(u32, u32)>>, line_number
     }
 
     gear_nums
-
-    // if gear_nums.len() == 2 {
-    //     gear_nums[0] * gear_nums[1]
-    // } else {
-    //     0
-    // }
 }
 
 fn get_gears_in_line(line_number: usize, col: usize, num_positions: &Vec<Vec<(u32, u32)>>, lines: &Vec<&str>, gear_nums: &mut Vec<u64>) {
@@ -130,46 +101,3 @@ fn get_gears_in_line(line_number: usize, col: usize, num_positions: &Vec<Vec<(u3
     }
 }
 
-
-fn is_part_number(line_number: usize, pos: (u32, u32), lines: &Vec<&str>) -> bool {
-
-    let mut i: i32 = (pos.0 as i32) - 1;
-
-    let line_length = lines[line_number].len();
-
-    if pos.0 > 0 && is_symbol(lines[line_number].chars().nth((pos.0 - 1) as usize)) {
-        return true;
-    }
-
-    if pos.1 + 1 < line_length as u32 && is_symbol(lines[line_number].chars().nth((pos.1 + 1) as usize)) {
-        return true;
-    }
-
-    while i <= (pos.1 as i32) + 1 {
-        // check above
-        if line_number > 0 {
-            if i >= 0 && i < line_length as i32 && is_symbol(lines[line_number - 1].chars().nth(i as usize)) {
-                return true;
-            }
-        }
-
-        // check below
-        if line_number < lines.len() - 1 {
-            if i >= 0 && i < line_length as i32 && is_symbol(lines[line_number + 1].chars().nth(i as usize)) {
-                return true;
-            }
-        }
-
-        i += 1;
-    }
-
-    false
-}
-
-fn is_symbol(chr: Option<char>) -> bool {
-    if let Some(ch) = chr {
-        return !ch.is_digit(10) && ch != '.'
-    }
-
-    false
-}
